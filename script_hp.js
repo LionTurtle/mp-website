ad2759f42c6632980a85eff92845bc199a18bd86
Mark Peng <mpeng@mpeng-mbp.local>
2016-04-19 12:49:54 -0700

    Bulk port of 2013 website to github.

---
+++ script_hp.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$('#left').hide();
+	$('#rightright').hide();
+	$('#email').hide();
+	$('#left').fadeIn({duration:4000,easing:"swing"});
+	$('#rightright').fadeIn({duration:4000,easing:"swing"});
+	$('#email').fadeIn({duration:4000,easing:"swing"});
+});

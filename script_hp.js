0c7b43051db3b87f46593a8a488050528e2db1de
Mark Peng <peng.markv@gmail.com>
2016-07-19 14:49:23 -0700

    Change homepage to circular layout

--- script_hp.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	$('#left').hide();
-	$('#rightright').hide();
-	$('#email').hide();
-	$('#left').fadeIn({duration:4000,easing:"swing"});
-	$('#rightright').fadeIn({duration:4000,easing:"swing"});
-	$('#email').fadeIn({duration:4000,easing:"swing"});
-});
